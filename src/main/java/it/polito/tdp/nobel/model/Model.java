ef9fed65d3f7c75a846866d46db90592f2158eb4
Giovanna Mariotto <s256530@studenti.polito.it>
2021-04-16 11:02:05 +0200

    Bad performance

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,16 +1,70 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class Model {
+import it.polito.tdp.nobel.db.EsameDAO;
 
+public class Model {
+	private List<Esame> partenza;
+	private Set<Esame> soluzioneMigliore;
+	private double mediaSoluzioneMigliore;
+	
+	public Model() {
+		EsameDAO dao = new EsameDAO();
+		this.partenza=dao.getTuttiEsami();
+	}
+	
 	
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
-		System.out.println("TODO!");
-		return null;	
+		Set<Esame> parziale = new HashSet<Esame>();
+		soluzioneMigliore = new HashSet<Esame>();
+		mediaSoluzioneMigliore=0;
+		
+		cerca(parziale,0,numeroCrediti);//funzione ricorsiva
+		
+			
+		return soluzioneMigliore;	
 	}
 
-	
+	/*COMPLESSITA: N! */
+	private void cerca(Set<Esame> parziale, int L, int m) {
+		//casi terminali
+		int crediti = sommaCrediti(parziale);
+		
+		if(crediti>m) { //controllare i crediti
+			
+		}
+		if(crediti==m) {
+			double media = calcolaMedia(parziale);
+			if(media>mediaSoluzioneMigliore) {
+				soluzioneMigliore = new HashSet<>(parziale); //sovrascrivere
+				mediaSoluzioneMigliore = media;
+			}
+			return; //non creare sotto-alberi inutili ( L>m)
+		}
+		
+		//siccuramente, crediti<m: 
+		
+		// 1) non ci sono più esami da aggiungere: L=m
+		if(L==partenza.size()) {
+			return;
+		}
+		
+		//generare i sotto-problemi
+		for(Esame e : partenza) { //lascio partenza come è
+			if(!parziale.contains(e)) {
+				parziale.add(e);
+				cerca(parziale,L+1,m);
+				parziale.remove(e); //backtracking
+			}
+		}
+		
+		
+	}
+
+
 	public double calcolaMedia(Set<Esame> esami) {
 		
 		int crediti = 0;
